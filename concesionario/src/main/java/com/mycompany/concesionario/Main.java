a73b6118a266b262f6f5980e15434df6023b55a3
David <David@David>
2019-03-03 22:58:02 -0500

    clase Main

--- concesionario/src/main/java/com/mycompany/concesionario/Main.java
+++ concesionario/src/main/java/com/mycompany/concesionario/Main.java
@@ -10,5 +10,8 @@ package com.mycompany.concesionario;
  * @author David
  */
 public class Main {
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
     
 }
